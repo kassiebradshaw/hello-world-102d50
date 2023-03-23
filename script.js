fa050f1cc1db0d124bfdff35b5dbe98208523fa6
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2023-03-23 11:41:18 -0700

    Class 7 demo

--- script.js
+++ script.js
@@ -1,90 +1,66 @@
-// JAVASCRIIIIIIPT!
+// // JAVASCRIIIIIIPT!
 
-// Basic commands
-// alert(); --> a pop-up box with a message
-// console.log(); --> show some information in the developer console
-// document.write(); --> put new content on the HTML page
-// prompt(); --> get information from the user
+// FUNCTION BASICS
 
-// ------------------------------------------------------------------------
-// What is a variable? A container for data/information
+//Structure of function DECLARATION (the way you'll do it in 102)
+// function addTwoNumbers(parameters){code to execute}
 
-// 4 ways to declare a variable
-// let <-- Yes
-// const <-- Yes
-// var <-- NO, not in 102
-// (nothing) <-- NO, not in 102
-
-// Example of declaring a CHANGEABLE VARIABLE
-// Variables declared with the 'let' keyword can have their value REASSIGNED (changed)
-let myName = 'Kassie';
-document.write(myName);
-// change the value
-myName = 'Kassandra';
-document.write(myName);
-
-// CONSTANT VARIABLES CAN NOT BE REASSIGNED
-const firstPet = "Hazel"
-// firstPet = "Chelsea" <-- trying to reassign the value would give us an error and stop our JavaScript.
-
-// ----------------------------------------------------
-
-// TWO WAYS YOU CAN ASSIGN VALUE TO A VARIABLE
-// declare a variable first
-let num;
-// then assign value to the variable
-num = 42;
-
-// OR...
-
-// declare a variable AND assign it a value at the same time
-let thisNum = 24;
-
-// ----------------------------------------------------
-
-// VARIABLE NAMING
-// variable names are typically camel-cased
-// each variable must have a unique identifier (name)
-let myFavColor = 'rainbow';
-let myOtherFavColor = 'yellow';
-
-// ----------------------------------------------------
-
-// DATA TYPES
-// strings --> TEXT --> Surrounded in quotes: 'Kassie', '2', 'two'
-// number --> 2, 42, 120
-// boolean --> TRUE or FALSE
-
-// ----------------------------------------------------
-
-// prompt the user for information
-let usersName = prompt("What is your name?");
-
-// ----------------------------------------------------
-
-// CONDITIONAL LOGIC
-// basic structure of conditional logic
-// if (this is true){execute the code in these brackets}
-// if (this is true){
-//  execute the code in these brackets
-// } else if (this is true instead of the previous one) {
-//  execute the code in these brackets
-// } else {for ANYTHING else not previously specified, execute the code in these brackets}
-
-if (usersName == 'Kassie'){
-  document.write("Hiya teach!!!!!");
-} else if (usersName == 'Justin'){
-  document.write("TA's are AMAZING");
-} else if (usersName == 'Kjell'){
-  document.write("TA's are AMAZING");
-} else {
-  document.write("Welcome to my page y'all!");
+// I'm declaring a function here (creating a command)
+function addTwoNumbers(number1, number2){
+  document.write(number1 + number2);
+  return number1 + number2;
 }
 
-// This next line will only happen after the conditional logic is done executing.
-document.write("        I like turtles");
+// Parameters live in parenthesis - this is extra information the function will expect
+// A return statement is how you can get information out of a function
 
-// An example of how you can turn a variable's value to all lowercase <-- extra!!!!
-// let usersName = 'KASSIE'
-// usersName = usersName.toLowerCase();
-// console.log('kassie' == usersName); 
+// ------------------------------------------------------------------------
+
+// Invoking/calling/using a command by name
+addTwoNumbers(1, 4);
+
+// You can also see where I'm using this function in the HTML, lines 44 & 79
+
+// ------------------------------------------------------------------------
+
+// If I want to access the result of a function, I need to store it in a new variable
+let myNewNumber = addTwoNumbers(10, 10); // myNewNumber holds the value of 20 returned from the function
+
+// ------------------------------------------------------------------------
+
+// Example of a function EXPRESSION <-- you'll also use this way later, but not in 102
+// let myFunction = function(){
+//   execute this code; 
+// }
+
+// ------------------------------------------------
+
+function greetUser(){
+  let usersName = prompt("What is your name?");
+  console.log(usersName);
+  
+  if (usersName == ""){
+    usersName = prompt("That's nothing, please put in your name!");
+  } else if (usersName == 'Kassie'){
+    document.write("Hiya teach!!!!!");
+  } else if (usersName == 'Justin'){
+    document.write("TA's are AMAZING");
+  } else if (usersName == 'Kjell'){
+    document.write("TA's are AMAZING");
+  } else {
+    document.write("Welcome to my page y'all!");
+  }
+  return usersName;
+}
+
+let theirName = greetUser();
+console.log(theirName);
+
+
+// let sum = addTwoNumbers(1, 4); // 5
+// addTwoNumbers(19, 60); // 79
+// addTwoNumbers("hello", "world"); // helloworld || hello-world || hello+world || error?
+// addTwoNumbers("19", 19); // Same as above?
+// addTwoNumbers(19, "hello"); // Same as above?
+
+// addTwoNumbers(42, 42);
